88ba768d516300f41825e672db305eb88226c982
CHO <choqu@outlook.com>
2021-10-31 01:06:14 +0900

    Initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-gray-100 min-h-screen pt-12">
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+    />
+    <NuxtPage class="max-w-xl bg-white shadow mx-auto border p-6 rounded" />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold mb-8">Hello World</h1>
+    <span class="font-normal">こんにちは</span>
+  </div>
+</template>
